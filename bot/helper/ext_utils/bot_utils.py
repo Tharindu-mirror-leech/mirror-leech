3c8ccf00c5848e75cfd6cdc7e1cad044c68768e3
tharindupraba <64023491+tharindupraba@users.noreply.github.com>
2022-08-22 20:27:43 +0530

    Update bot_utils.py
    Signed-off-by: tharindupraba <64023491+tharindupraba@users.noreply.github.com>

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -130,12 +130,12 @@ def get_readable_message():
             #msg += f"<b><a href='{download.message.link}'>{download.status()}</a>: </b>"
             if download.status() not in [MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_SEEDING]:
                # msg += f"\n{get_progress_bar_string(download)} {download.progress()}"
+                msg += f"📌Name:<code>{escape(str(download.name()))}</code>"
                 msg += f"\n<b>📡 Pʀᴏᴄᴇssᴇᴅ:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
                 msg += f"\n<b>🚀 Sᴘᴇᴇᴅ:</b> {download.speed()} | <b>💦 Eᴛᴀ:</b> {download.eta()}"
                 if hasattr(download, 'seeders_num'):
                     try:
-                        msg += f"\n<b>🍃 Sᴇᴇᴅᴇʀs:</b> {download.seeders_num()} | <b>💬 Lᴇᴇᴄʜᴇʀs:</b> {download.leechers_num()}"\n
-                        msg += f"📌Name:<code>{escape(str(download.name()))}</code>"
+                        msg += f"\n<b>🍃 Sᴇᴇᴅᴇʀs:</b> {download.seeders_num()} | <b>💬 Lᴇᴇᴄʜᴇʀs:</b> {download.leechers_num()}"
                     except:
                         pass
                 
