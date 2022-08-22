09ee0b1da769da2bed37a41ab9710988289715e7
tharindupraba <64023491+tharindupraba@users.noreply.github.com>
2022-08-22 18:17:32 +0530

    Update bot_utils.py
    Signed-off-by: tharindupraba <64023491+tharindupraba@users.noreply.github.com>

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -125,7 +125,7 @@ def get_readable_message():
         for index, download in enumerate(list(download_dict.values())[COUNT:], start=1):
             msg += f"<b><a href='{download.message.link}'>{download.status()}</a> </b>"
             msg += f"\n<b>{get_progress_bar_string(download)} {download.progress()}</b>"
-            msg += f"\n<b> ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ </b>"
+            msg += f"\n<b> ━━━━━━━━━━━━━━━━━━━━━━━━━━━━ </b>"
             #msg += f"🍻<code>{escape(str(download.name()))}</code>"
             #msg += f"<b><a href='{download.message.link}'>{download.status()}</a>: </b>"
             if download.status() not in [MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_SEEDING]:
