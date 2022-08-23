e60a501f48a1f6aab9a67ad7edfe59c42320942a
tharindupraba <64023491+tharindupraba@users.noreply.github.com>
2022-08-23 21:50:48 +0530

    Update bot_utils.py
    Signed-off-by: tharindupraba <64023491+tharindupraba@users.noreply.github.com>

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -136,9 +136,9 @@ def get_readable_message():
                         msg += f"\n<b>┃  🍃 Sᴇᴇᴅᴇʀs➽ </b> {download.seeders_num()} | <b>💬 Lᴇᴇᴄʜᴇʀs➽ </b> {download.leechers_num()}"
                     except:
                         pass
-                msg += f"\n┃📌Name➽ <code>{escape(str(download.name()))}</code>"
+                msg += f"\n┃ 📌Name➽ <code>{escape(str(download.name()))}</code>"
                 #msg += f"\n<b> ━━━━━━━━━━━━━━━━━━━━━━━━━━━━ </b>"
-                msg += f"\n<b> ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫ </b>"
+                msg += f"\n<b>┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫ </b>"
             elif download.status() == MirrorStatus.STATUS_SEEDING:
                 msg += f"\n<b>┃  📦 𝕊𝕚𝕫𝕖➽ </b>{download.size()}"
                 msg += f"\n<b>┃ 📯 𝕊𝕡𝕖𝕖𝕕➽ </b>{download.upload_speed()}"
