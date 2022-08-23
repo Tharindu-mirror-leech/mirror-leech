870142f1e80c94d967e225f667b6b0ec842e9b0b
tharindupraba <64023491+tharindupraba@users.noreply.github.com>
2022-08-23 20:41:34 +0530

    Update bot_utils.py
    Signed-off-by: tharindupraba <64023491+tharindupraba@users.noreply.github.com>

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -123,6 +123,7 @@ def get_readable_message():
                 globals()['PAGE_NO'] -= 1
             
         for index, download in enumerate(list(download_dict.values())[COUNT:], start=1):
+            msg += "\n"
             msg += f"<b><a href='{download.message.link}'>{download.status()}</a> </b>"
             msg += f"\n<b>{get_progress_bar_string(download)} {download.progress()}</b>"
             msg += f"\n<b> ━━━━━━━━━━━━━━━━━━━━━━━━━━━━ </b>"
