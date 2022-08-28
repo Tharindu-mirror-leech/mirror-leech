0101aa0558d5eae114ebc7deeb7e1fcabe8fa103
tharindupraba <64023491+tharindupraba@users.noreply.github.com>
2022-08-29 00:49:29 +0530

    update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -106,7 +106,7 @@ def get_progress_bar_string(status):
     p = 0 if total == 0 else round(completed * 100 / total)
     p = min(max(p, 0), 100)
     cFull = p // 8
-    p_str = '🟥' * cFull
+    p_str = '🟦' * cFull
     p_str += '⬜️' * (10 - cFull)
     p_str = f"{p_str}"
     return p_str
