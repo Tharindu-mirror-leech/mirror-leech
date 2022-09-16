3a05b8aa0608e64677b3fc6dd1cdb28592049c7a
tharindupraba <64023491+tharindupraba@users.noreply.github.com>
2022-09-16 11:21:20 +0530

    update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -124,7 +124,7 @@ def get_readable_message():
             
         for index, download in enumerate(list(download_dict.values())[COUNT:], start=1):
             #msg += "\n"
-            msg += f"\n<b>┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓</b>"
+            msg += f"\n<b>┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓</b>"
             msg += f"\n<b>┃  <a  href='{download.message.link}'>{download.status()}</a></b>"
             msg += f"\n┃  {get_progress_bar_string(download)} {download.progress()}"
             #msg += f"\n<b>┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓ </b>"
