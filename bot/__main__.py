351491bddc17e0bbbb86cf8cf4dcb4d13cda5ded
tharindupraba <64023491+tharindupraba@users.noreply.github.com>
2022-09-23 00:47:37 +0530

    update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -64,7 +64,7 @@ def stats(update, context):
 def start(update, context):
     buttons = ButtonMaker()
     buttons.buildbutton("🔥 Repo", "https://github.com/Tharindu-mirror-leech/mirror-leech")
-    buttons.buildbutton("😎 Channel", "https://t.me/+R2czcPeMkUc1NDI1")
+    buttons.buildbutton("😎 Channel", "https://t.me/hkomhv")
     reply_markup = buttons.build_menu(2)
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
